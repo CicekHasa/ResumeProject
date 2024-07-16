ffb78428c9ff10716c1962d135b326f64b755716
CicekHasa <hasancicek2001ist@gmail.com>
2024-07-16 22:30:55 +0300

    Statistic controller'ı eklendi.
    ViewBag yapısı öğrenilip uygulandı.
    
    Procedure kullanımı öğrenildi ve Db de oluşturuldu. Oluşturulan prosedür kod tarafında kullanıldı.
    
    Sitenin ön yüzü koda eklendi.
    
    Default controller açılıp orada oluşturulan view'e eklendi.

---
+++ ResumeProject/ResumeProject/Controllers/StatisticController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResumeProject.Models;
+namespace ResumeProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            //ViewBag.skillCount = db.TblSkill.Count();//tablodaki toplam kayıt sayısını verir.
+            ViewBag.countProjeTalebi = db.CountProjeTalebi().FirstOrDefault();//Db de oluşturduğum prosedürümü models'e ekleyip kullandım.
+            ViewBag.technologyCount = db.TblTechnology.Count();
+            ViewBag.csharpValue = db.TblTechnology.Where(X => X.TechnologyTitle == "C#").Select(y => y.TechnologyValue).FirstOrDefault();//başlık c# olan satırın value'sini getir. Değeri döndürmesi için ya ToList() ya da firstOrDefault() olmalı!!! 
+            ViewBag.contactCount = db.TblContact.Count();
+            ViewBag.subjectTesekkur = db.TblContact.Where(x => x.Subject == 1).Count();
+            ViewBag.sumTechnologyValue = db.TblTechnology.Sum(x => x.TechnologyValue);
+            ViewBag.averageTechnologyValue = db.TblTechnology.Average(x => x.TechnologyValue);
+            ViewBag.getBy3Id = db.TblSkill.Where(x => x.SkillID == 3).Select(y => y.SkillTitle).FirstOrDefault();
+            ViewBag.maxTechnologyValue=db.TblTechnology.Max(x => x.TechnologyValue);
+            return View();
+        }
+    }
+}
