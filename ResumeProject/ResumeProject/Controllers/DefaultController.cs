d7fae2d0a8f15191c2affea9556429a2c99f7bd7
CicekHasa <hasancicek2001ist@gmail.com>
2024-07-17 22:44:18 +0300

    Partial yapısı öğrenildi.
    Sitenin ön yüzündeki ana sayfa olan Index sayfasını partialara ayırıldı.
    
    Bazı partiallar db deki tablodan dinamik olarak çekildi.
    
    TblService adında yeni bir tablo eklenip model kısmında güncellendi.

--- ResumeProject/ResumeProject/Controllers/DefaultController.cs
+++ ResumeProject/ResumeProject/Controllers/DefaultController.cs
@@ -3,15 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ResumeProject.Models;
 namespace ResumeProject.Controllers
 {
     public class DefaultController : Controller
     {
-        
+        DbResumeEntities db=new DbResumeEntities();
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialBrands()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialProjects()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialCounter()
+        {
+            ViewBag.skillCount = db.TblSkill.Count();
+            ViewBag.serviceCount = db.TblService.Count();
+            ViewBag.avgTechnologyValue = ViewBag.avgTechnologyValue = Math.Round(db.TblTechnology.Average(x => x.TechnologyValue).GetValueOrDefault());
+            ViewBag.happyCustomer = 38;
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTechnology()
+        {
+            var values = db.TblTechnology.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialService()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblProfile.ToList(); 
+            return PartialView(values);
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
+
+       
     }
 }
